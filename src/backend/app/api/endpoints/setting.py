b5383ed970ee009f11d1b9765584506e2120a89a
Ilya Malashenko <melax08@yandex.ru>
2025-04-01 18:05:53 +0300

    refactor(api.settings): move business logic of settings endpoint to the service layer

--- src/backend/app/api/endpoints/setting.py
+++ src/backend/app/api/endpoints/setting.py
@@ -1,37 +1,28 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from backend.app.api.validators import check_currency_exists
-from backend.app.core.db import get_async_session
-from backend.app.crud import setting_crud
 from backend.app.schemas.setting import Budget, DefaultCurrency, SettingDB
+from backend.app.services.setting import SettingsService
 
 router = APIRouter()
 
 
 @router.get("/", response_model=SettingDB)
-async def get_settings(session: AsyncSession = Depends(get_async_session)) -> SettingDB:
-    """Get first settings instance with all settings."""
-    settings = await setting_crud.get_settings(session)
-    return settings
+async def get_settings(settings_service: SettingsService = Depends(SettingsService)) -> SettingDB:
+    """Get the first settings instance with all settings."""
+    return await settings_service.get_settings()
 
 
 @router.post("/set-default-currency", response_model=SettingDB)
 async def set_default_currency(
-    currency: DefaultCurrency, session: AsyncSession = Depends(get_async_session)
+    currency: DefaultCurrency, settings_service: SettingsService = Depends(SettingsService)
 ) -> SettingDB:
-    """Set new default currency of application."""
-    currency = await check_currency_exists(currency.currency_id, session)
-    settings = await setting_crud.get_settings(session)
-    settings = await setting_crud.set_default_currency(settings, currency.id, session)
-    return settings
+    """Set new default currency of the application."""
+    return await settings_service.set_default_currency(currency)
 
 
 @router.post("/set-budget", response_model=SettingDB)
 async def set_budget(
-    budget: Budget, session: AsyncSession = Depends(get_async_session)
+    budget: Budget, settings_service: SettingsService = Depends(SettingsService)
 ) -> SettingDB:
-    """Set new application budget."""
-    settings = await setting_crud.get_settings(session)
-    settings = await setting_crud.set_budget(settings, budget.budget, session)
-    return settings
+    """Set the new application monthly budget."""
+    return await settings_service.set_budget(budget.budget)
---
+++ src/backend/app/services/setting.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+
+from backend.app.api.validators import check_currency_exists
+from backend.app.crud import setting_crud
+from backend.app.schemas.setting import DefaultCurrency, SettingDB
+from backend.app.services.base import BaseService
+
+
+class SettingsService(BaseService):
+    """Service to manage application settings."""
+
+    async def get_settings(self) -> SettingDB:
+        """Get actual application settings."""
+        return await setting_crud.get_settings(self._session)
+
+    async def set_default_currency(self, currency: DefaultCurrency) -> SettingDB:
+        """Set a new default application currency."""
+        currency = await check_currency_exists(currency.currency_id, self._session)
+        settings = await setting_crud.get_settings(self._session)
+        return await setting_crud.set_default_currency(settings, currency.id, self._session)
+
+    async def set_budget(self, budget_amount: Decimal) -> SettingDB:
+        """Set a new monthly application budget."""
+        settings = await setting_crud.get_settings(self._session)
+        return await setting_crud.set_budget(settings, budget_amount, self._session)
